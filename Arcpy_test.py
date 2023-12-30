fab0fe11c17760b4a0f3771e234a3f903fba977b
Lavanya Gopal <lavgopal9@gmail.com>
2023-12-30 00:16:01 -0800

    jupyter on vscode: kernel not starting

--- Arcpy_test.py
+++ Arcpy_test.py
@@ -1,4 +1,5 @@
 import arcpy
+
 arcpy.env.workspace=r"C:\viu\giscoursework\531\wk9\Lab9_Data\NanaimoGDB.gdb"
 folder=r"C:\viu\giscoursework\531\wk9\Lab9_Data"
 MyAprx=arcpy.mp.ArcGISProject(folder+r"\Assignment10\Assignment10.aprx")
